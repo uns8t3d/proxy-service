9efc239b5988a5cba6a20b4a27a3b5916d65b599
ZagrebinB <zagrebin.bogdan@gmail.com>
2017-02-25 23:49:52 +0200

    Move to admin lte

--- proxyserver/settings/default.py
+++ proxyserver/settings/default.py
@@ -115,7 +115,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-    '/opt/proxy/proxy-service/proxyserver/static_root',
+    '/opt/proxy/proxy-service/proxyserver/static',
 )
 
 STATICFILES_FINDERS = (
