08d9ecb5c18a1891c44ea04cb38020b06d58c791
ZagrebinB <zagrebin.bogdan@gmail.com>
2017-03-04 17:02:59 +0200

    Move to admin lte

--- proxyserver/apps/api/views.py
+++ proxyserver/apps/api/views.py
@@ -1,6 +1,5 @@
 from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 from proxyserver.apps.core.models import Proxy
-from .models import UserProxies
 from .permissions import IsOwnerOrReadOnly
 from .serializers import ProxySerializer
 from rest_framework import permissions, renderers, viewsets
@@ -27,15 +26,3 @@ def api_root(request, format=None):
     return Response({
         'proxies': reverse('proxy-list', request=request, format=format)
     })
-
-
-class UserProxyList(APIView):
-    renderer_classes = (JSONRenderer, BrowsableAPIRenderer,)
-
-    def get(self, request, format=None):
-        # proxy = Proxy.objects.order_by('?').filter(status='Available')[0]
-        for proxy in Proxy.objects.order_by('?').filter(status='Available'):
-            if not UserProxies.objects.filter(user_id=self.request.user, proxy=proxy):
-                UserProxies.objects.create(user_id=self.request.user, proxy=proxy)
-                content = {'port': proxy.port, 'ip': proxy.ip_address}
-                return Response(content)
