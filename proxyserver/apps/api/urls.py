4d1a613fa56dd2ea7976c4ba793b411af56eb0ff
ZagrebinB <zagrebin.bogdan@gmail.com>
2017-03-04 17:03:56 +0200

    Move to admin lte

--- proxyserver/apps/api/urls.py
+++ proxyserver/apps/api/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url, include
 from . import views
-from django.contrib.auth.decorators import login_required
 from rest_framework.routers import DefaultRouter
 
 
@@ -10,6 +9,5 @@ router.register(r'proxies', views.ProxyViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^get_proxy/$', login_required(views.UserProxyList.as_view())),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
